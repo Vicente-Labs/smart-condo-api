91fb0f3e208a4993ad9b0e9aa59cc756952bf177
Vicentesan <vikom.sanchez@gmail.com>
2024-12-08 04:21:58 -0300

    revert last commit

--- src/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
-
-import { env } from '../env'
-import * as schema from './schemas'
-
-export const db = drizzle(env.DATABASE_URL, {
-  schema,
-  logger: env.NODE_ENV !== 'production',
-})
