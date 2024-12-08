1c473812db5043feb1a79b40dec735fecbab19ef
Vicentesan <vikom.sanchez@gmail.com>
2024-12-08 07:17:46 -0300

    feat: add setters

--- src/domain/entities/resident.ts
+++ src/domain/entities/resident.ts
@@ -24,24 +24,58 @@ export class Resident extends Entity<ResidentProps> {
     return resident
   }
 
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
   get name() {
     return this.props.name
+  }
+
+  set name(name: string) {
+    this.props.name = name
+
+    this.touch()
   }
 
   get email() {
     return this.props.email
   }
 
+  set email(email: string) {
+    this.props.email = email
+
+    this.touch()
+  }
+
   get phone() {
     return this.props.phone
   }
 
+  set phone(phone: string) {
+    this.props.phone = phone
+
+    this.touch()
+  }
+
   get avatarUrl() {
-    return this.props.avatarUrl
+    return this.props.avatarUrl ?? null
+  }
+
+  set avatarUrl(avatarUrl: string | null) {
+    this.props.avatarUrl = avatarUrl
+
+    this.touch()
   }
 
   get condominiumId() {
     return this.props.condominiumId
+  }
+
+  set condominiumId(condominiumId: UniqueEntityId) {
+    this.props.condominiumId = condominiumId
+
+    this.touch()
   }
 
   get createdAt() {
